c5367a29368282acfc2ab622a3ddff41ecc210fb
ScornSan <yohan.marcel92@gmail.com>
2021-12-18 11:35:17 +0100

    fonction dé et rejouez si double, jusqu'a prison

--- Fonction déplacements/fonction dé/main.c
+++ Fonction déplacements/fonction dé/main.c
@@ -4,10 +4,14 @@
 #define MAX_DE 6
 #define MIN_DE 1
 
-int aller_en_prison()
+int aller_en_prison(int* a, int* b)
 {
-    int compteur_prison = 0;
-    printf("On a fait un double\n");
+    int compteur_prison;
+    int somme_de = 0;
+    printf("Vous avez fait double");
+    sleep(3);
+    *a = (rand() % (MAX_DE-MIN_DE + 1) + MIN_DE);
+    *b = (rand() % (MAX_DE-MIN_DE + 1) + MIN_DE);
     compteur_prison++;
     if (compteur_prison == 3)
     {
@@ -15,25 +19,24 @@ int aller_en_prison()
         compteur_prison = 0;
         // appelle de la fonction prison
     }
-
     return 0;
 }
 
-int fct_de()
+int lancer_de()
 {
     // variables
     int val_de1 = 0;
     int val_de2 =  0;
-    int compteur_prison = 0;
     srand(time(NULL)); // initialisation générateur de nombres
 
     // génération de deux dés
     val_de1 = (rand() % (MAX_DE-MIN_DE + 1) + MIN_DE);
     val_de2 = (rand() % (MAX_DE-MIN_DE + 1) + MIN_DE);
     printf("%d\n%d\n", val_de1, val_de2);
-    if (val_de1 == val_de2)
+    while (val_de1 == val_de2)
     {
-        aller_en_prison();
+        aller_en_prison(&val_de1, &val_de2);
+        printf("%d", val_de1);
     }
     return val_de1 + val_de2;
 }
@@ -41,7 +44,7 @@ int fct_de()
 int main()
 {
     int a = 0;
-    a = fct_de();
-    printf("%d", a);
+    a = lancer_de();
+    printf("%d\n\n", a);
     return 0;
 }
