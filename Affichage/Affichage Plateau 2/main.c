d3a43a0fefa917042e3a1e26a66e768b6b970f06
benjamin0301 <gendrybenjamin@gmail.com>
2021-12-23 19:43:47 +0100

    affichage plateau

--- Affichage/Affichage Plateau 2/main.c
+++ Affichage/Affichage Plateau 2/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#define largeur_case 13
+#define hauteur_case 5
+#define derniere_colonne 104
+#define premiere_colonne 0
+#define deuxieme_colonne 13
+#define deuxieme_ligne 15
+#define avant_derniere_colonne 91
+#define avant_derniere_colonne_gain 92
+#define avant_derniere_ligne 45
+#define avant_derniere_ligne_gain 46
+#define derniere_ligne 50
+#define premiere_ligne_gain 11
+#define premiere_ligne 10
+#define troisieme_ligne 20
+#define nombre_lignes_a_faire 2
+#define nombre_lignes_a_faire_gain 4
+#define nombre_lignes_gain 30
+#define nombre_colonnes_gain 12
+#define jusque_repetition_ligne 39
+#define jusque_repetition_colonne 103
+
 
 void Color(int cT,int cF) // fonction d'affichage de couleurs
 {
@@ -24,272 +45,276 @@ SetConsoleCursorPosition(H, mycoord );
 
 void details()
 {
-    Color(7,15);
-    gotoligcol(58,112);
+    Color(11,15);
+    gotoligcol(derniere_ligne,derniere_colonne);
     printf("%c",0xBC);
     Color(0,15);
-    gotoligcol(58,0);
+    gotoligcol(derniere_ligne,premiere_colonne);
     printf("%c",0xC8);
     Color(10,15);
-    gotoligcol(10,0);
+    gotoligcol(premiere_ligne,premiere_colonne);
     printf("%c",0xC9);
     Color(12,15);
-    gotoligcol(10,112);
+    gotoligcol(premiere_ligne,derniere_colonne);
     printf("%c",0xBB);
 
     Color(10,15);
-    gotoligcol(16,14);
+    gotoligcol(deuxieme_ligne,deuxieme_colonne);
     printf("%c",0xBC);
     Color(12,15);
-    gotoligcol(16,98);
+    gotoligcol(deuxieme_ligne,avant_derniere_colonne);
     printf("%c",0xC8);
     Color(0,15);
-    gotoligcol(52,14);
+    gotoligcol(avant_derniere_ligne,deuxieme_colonne);
     printf("%c",0xBB);
-    Color(7,15);
-    gotoligcol(52,98);
+    Color(11,15);
+    gotoligcol(avant_derniere_ligne,avant_derniere_colonne);
     printf("%c",0xC9);
 
-    Color(7,15);
-    gotoligcol(52,112);
+    Color(11,15);
+    gotoligcol(avant_derniere_ligne,derniere_colonne);
     printf("%c",0xBB);
     Color(0,15);
-    gotoligcol(52,0);
+    gotoligcol(avant_derniere_ligne,premiere_colonne);
     printf("%c",0xC9);
     Color(10,15);
-    gotoligcol(10,14);
+    gotoligcol(premiere_ligne,deuxieme_colonne);
     printf("%c",0xBB);
     Color(12,15);
-    gotoligcol(10,98);
+    gotoligcol(premiere_ligne,avant_derniere_colonne);
     printf("%c",0xC9);
 
     Color(10,15);
-    gotoligcol(16,0);
+    gotoligcol(deuxieme_ligne,premiere_colonne);
     printf("%c",0xC8);
     Color(12,15);
-    gotoligcol(16,112);
+    gotoligcol(deuxieme_ligne,derniere_colonne);
     printf("%c",0xBC);
     Color(0,15);
-    gotoligcol(58,14);
+    gotoligcol(derniere_ligne,deuxieme_colonne);
     printf("%c",0xBC);
-    Color(7,15);
-    gotoligcol(58,98);
+    Color(11,15);
+    gotoligcol(derniere_ligne,avant_derniere_colonne);
     printf("%c",0xC8);
 }
 
+
 void cases_speciales_lignes()
 {
-    int ligne = 10;
-    for (int i = 0; i<2; i++){
+    int ligne = premiere_ligne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
         int col = 1;
-        for (int j = 0; j<13; j++)
+        for (int j = 0; j<nombre_colonnes_gain; j++)
             {
                 Color(10,15);
                 gotoligcol(ligne,col);
-                col = col +1;
+                col++;
                 printf("%c", 0xCD);
             }
-        ligne = ligne + 6;
+        ligne = ligne + hauteur_case;
     }
-    ligne = 10;
-    for (int i = 0; i<2; i++){
-        int col = 99;
-        for (int j = 0; j<13; j++)
+    ligne = premiere_ligne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
+        int col = avant_derniere_colonne_gain;
+        for (int j = 0; j<nombre_colonnes_gain; j++)
             {
                 Color(12,15);
                 gotoligcol(ligne,col);
-                col = col +1;
+                col++;
                 printf("%c", 0xCD);
             }
-        ligne = ligne + 6;
+        ligne = ligne + hauteur_case;
     }
-    ligne = 52;
-    for (int i = 0; i<2; i++){
+    ligne = avant_derniere_ligne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
         int col = 1;
-        for (int j = 0; j<13; j++)
+        for (int j = 0; j<nombre_colonnes_gain; j++)
             {
                 Color(0,15);
                 gotoligcol(ligne,col);
-                col = col +1;
+                col++;
                 printf("%c", 0xCD);
             }
-        ligne = ligne + 6;
+        ligne = ligne + hauteur_case;
     }
-    ligne = 52;
-    for (int i = 0; i<2; i++){
-        int col = 99;
-        for (int j = 0; j<13; j++)
+    ligne = avant_derniere_ligne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
+        int col = avant_derniere_colonne_gain;
+        for (int j = 0; j<nombre_colonnes_gain; j++)
             {
-                Color(7,15);
+                Color(11,15);
                 gotoligcol(ligne,col);
-                col = col +1;
+                col++;
                 printf("%c", 0xCD);
             }
-        ligne = ligne + 6;
+        ligne = ligne + hauteur_case;
     }
 }
 
 void cases_speciales_colonnes()
 {
-    int col = 0;
-    for (int i = 0; i<2; i++){
-        int lig = 11;
-        for (int j = 0; j<5; j++)
+    int col = premiere_colonne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
+        int lig = premiere_ligne_gain;
+        for (int j = 0; j<nombre_lignes_a_faire_gain; j++)
             {
                 Color(10,15);
                 gotoligcol(lig,col);
-                lig = lig +1;
+                lig++;
                 printf("%c", 0xBA);
             }
-        col = col + 14;
+        col = col + largeur_case;
     }
-    col = 98;
-    for (int i = 0; i<2; i++){
-        int lig = 53;
-        for (int j = 0; j<5; j++)
+    col = avant_derniere_colonne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
+        int lig = avant_derniere_ligne_gain;
+        for (int j = 0; j<nombre_lignes_a_faire_gain; j++)
             {
-                Color(7,15);
+                Color(11,15);
                 gotoligcol(lig,col);
-                lig = lig +1;
+                lig++;
                 printf("%c", 0xBA);
             }
-        col = col + 14;
+        col = col + largeur_case;
     }
-    col = 98;
-    for (int i = 0; i<2; i++){
-        int lig = 11;
-        for (int j = 0; j<5; j++)
+    col = avant_derniere_colonne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
+        int lig = premiere_ligne_gain;
+        for (int j = 0; j<nombre_lignes_a_faire_gain; j++)
             {
                 Color(12,15);
                 gotoligcol(lig,col);
-                lig = lig +1;
+                lig++;
                 printf("%c", 0xBA);
             }
-        col = col + 14;
+        col = col + largeur_case;
     }
-    col = 0;
-    for (int i = 0; i<2; i++){
-        int lig = 53;
-        for (int j = 0; j<5; j++)
+    col = premiere_colonne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
+        int lig = avant_derniere_ligne_gain;
+        for (int j = 0; j<nombre_lignes_a_faire_gain; j++)
             {
                 Color(0,15);
                 gotoligcol(lig,col);
-                lig = lig +1;
+                lig++;
                 printf("%c", 0xBA);
             }
-        col = col + 14;
+        col = col + largeur_case;
     }
 }
+
+
 void sepa_lignes()
 {
-    int ligne = 10;
-    for (int i = 0; i<2; i++){
-        int col = 0;
-        for (int j = 0; j<112; j++)
+    int ligne = premiere_ligne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
+        int col = 1;
+        for (int j = 0; j<jusque_repetition_colonne; j++)
             {
                 Color(9,15);
                 gotoligcol(ligne,col);
-                col = col +1;
+                col++;
                 printf("%c", 0xCD);
             }
-        ligne = ligne + 6;
+        ligne = ligne + hauteur_case;
     }
-    ligne = 22;
-    for (int i = 0; i<5; i++){
-        int col = 0;
-        for (int j = 0; j< 14; j++)
+    ligne = troisieme_ligne;
+    for (int i = 0; i<hauteur_case; i++){
+        int col = 1;
+        for (int j = 0; j< largeur_case; j++)
             {
                 Color(9,15);
                 gotoligcol(ligne,col);
-                col = col +1;
+                col++;
                 printf("%c", 0xCD);
             }
-        ligne = ligne + 6;
+        ligne = ligne + hauteur_case;
     }
-    ligne = 22;
-    for (int i = 0; i<5; i++){
-        int col = 98;
-        for (int j = 0; j< 14; j++)
+    ligne = troisieme_ligne;
+    for (int i = 0; i<hauteur_case; i++){
+        int col = avant_derniere_colonne;
+        for (int j = 0; j< largeur_case; j++)
             {
                 Color(9,15);
                 gotoligcol(ligne,col);
-                col = col +1;
+                col++;
                 printf("%c", 0xCD);
             }
-        ligne = ligne + 6;
+        ligne = ligne + hauteur_case;
     }
-    ligne = 52;
-    for (int i = 0; i<2; i++){
-        int col = 0;
-        for (int j = 0; j<112; j++)
+    ligne = avant_derniere_ligne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
+        int col = 1;
+        for (int j = 0; j<jusque_repetition_colonne; j++)
             {
                 Color(9,15);
                 gotoligcol(ligne,col);
-                col = col +1;
+                col++;
                 printf("%c", 0xCD);
             }
-        ligne = ligne + 6;
+        ligne = ligne + hauteur_case;
     }
 }
 
-void sepa_colonnes()
+/*void sepa_colonnes()
 {
-    int col = 0;
-    for (int i = 0; i<2; i++){
-        int lig = 11;
-        for (int j = 0; j<47; j++)
+    int col = premiere_colonne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
+        int lig = premiere_ligne_gain;
+        for (int j = 0; j<jusque_repetition_ligne; j++)
             {
                 Color(9,15);
                 gotoligcol(lig,col);
-                lig = lig +1;
+                lig++;
                 printf("%c", 0xBA);
             }
-        col = col + 14;
+        col = col + largeur_case;
     }
-    col = 28;
-    for (int i = 0; i<5; i++){
-        int lig = 11;
-        for (int j = 0; j<5; j++)
+    col = 26;
+    for (int i = 0; i<hauteur_case; i++){
+        int lig = premiere_ligne_gain;
+        for (int j = 0; j<nombre_lignes_a_faire_gain; j++)
             {
                 Color(9,15);
                 gotoligcol(lig,col);
-                lig = lig +1;
+                lig++;
                 printf("%c", 0xBA);
             }
-        col = col + 14;
+        col = col + largeur_case;
     }
-    col = 28;
-    for (int i = 0; i<5; i++){
-        int lig = 53;
-        for (int j = 0; j<5; j++)
+    col = 26;
+    for (int i = 0; i<hauteur_case; i++){
+        int lig = avant_derniere_ligne_gain;
+        for (int j = 0; j<nombre_lignes_a_faire_gain; j++)
             {
                 Color(9,15);
                 gotoligcol(lig,col);
-                lig = lig +1;
+                lig++;
                 printf("%c", 0xBA);
             }
-        col = col + 14;
+        col = col + largeur_case;
     }
-    col = 98;
-    for (int i = 0; i<2; i++){
-        int lig = 11;
-        for (int j = 0; j<47; j++)
+    col = avant_derniere_colonne;
+    for (int i = 0; i<nombre_lignes_a_faire; i++){
+        int lig = premiere_ligne_gain;
+        for (int j = 0; j<jusque_repetition_ligne; j++)
             {
                 Color(9,15);
                 gotoligcol(lig,col);
-                lig = lig +1;
+                lig++;
                 printf("%c", 0xBA);
             }
-        col = col + 14;
+        col = col + largeur_case;
     }
-}
+}*/
+
 void lignes()
 {
-    int lig = 10;
-    for(int i = 0; i < 6; i++)
+    int lig = premiere_ligne;
+    for(int i = 0; i < hauteur_case; i++)
     {
-        int col = 0;
-        for (int j = 0; j< 112; j++)
+        int col = 1;
+        for (int j = 0; j< jusque_repetition_colonne; j++)
             {
                 Color(0,15);
                 gotoligcol(lig,col);
@@ -298,11 +323,11 @@ void lignes()
             }
         lig++;
     }
-    lig = 52;
-    for(int i = 0; i < 6; i++)
+    lig = avant_derniere_ligne;
+    for(int i = 0; i < hauteur_case; i++)
     {
-        int col = 0;
-        for (int j = 0; j< 112; j++)
+        int col = premiere_colonne;
+        for (int j = 0; j< derniere_colonne; j++)
             {
                 Color(0,15);
                 gotoligcol(lig,col);
@@ -313,13 +338,13 @@ void lignes()
     }
 }
 
-void colonnes()
+/*void colonnes()
 {
-    int col = 0;
-    for(int i = 0; i < 14; i++)
+    int col = premiere_colonne;
+    for(int i = 0; i < largeur_case; i++)
     {
-        int lig = 16;
-        for (int j = 0; j< 36; j++)
+        int lig = deuxieme_ligne;
+        for (int j = 0; j< nombre_lignes_gain; j++)
             {
                 Color(0,15);
                 gotoligcol(lig,col);
@@ -328,11 +353,11 @@ void colonnes()
             }
         col++;
     }
-    col = 98;
-    for(int i = 0; i < 14; i++)
+    col = avant_derniere_colonne;
+    for(int i = 0; i < largeur_case; i++)
     {
-        int lig = 16;
-        for (int j = 0; j< 36; j++)
+        int lig = deuxieme_ligne;
+        for (int j = 0; j< nombre_lignes_gain; j++)
             {
                 Color(0,15);
                 gotoligcol(lig,col);
@@ -341,7 +366,7 @@ void colonnes()
             }
         col++;
     }
-}
+}*/
 
 int main()
 {   //passage en plein écran --> écran passe de 120 a 180 colonnes
@@ -351,22 +376,21 @@ int main()
     // prog test taille ecran
     /*for (int i = 0; i< 180; i++){
             Color(0,15);
-            gotoligcol(10,col);
+            gotoligcol(premiere_ligne,col);
             col = col +1;
             printf("%c", " " );
         }*/
     lignes();
-    colonnes();
+    //colonnes();
     sepa_lignes();
-    sepa_colonnes();
+    //sepa_colonnes();
     details();
     cases_speciales_lignes();
     cases_speciales_colonnes();
-    gotoligcol(70,0);
+    gotoligcol(52,premiere_colonne);
     return 0;
 
 }
-
 /*
     0 : Noir
     1 : Bleu foncé
