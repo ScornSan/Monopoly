b97b3a9b9b6376e038dd75197b6bd572b3ec381f
ScornSan <yohan.marcel92@gmail.com>
2021-12-25 19:17:50 +0100

    ajout hypotheque et vente tour joueur v1

--- Projet final/Projet/Tour_joueur/Tour_joueur.c
+++ Projet final/Projet/Tour_joueur/Tour_joueur.c
@@ -5,101 +5,79 @@ int tour_joueur(t_joueur joueur_i[], int nombre_joueurs, int id_joueur, t_carte 
 {
     int de1, de2;
     int nb_lancer = 0;
-    /// vérification tour suivant ou pas
-    while ((nb_lancer == 0) || ((de1 == de2)))
+    /// On demande au joueur les autres actions qu'il veut effectuer avant de lancer les dés et commencer le tour
+    printf("C'est a %s de jouer !\n"
+           "Appuyez sur ESPACE pour lancer les des\n"
+           "Appuyez sur 'V' pour vendre des maisons\n"
+           "Appuyez sur 'H' pour hypothequer des terrains\n", joueur_i[id_joueur].pseudo);
+    switch (getch())
     {
-        printf("C'est au joueur %d de jouer ! Appuyez sur ESPACE pour lancer les des\n", id_joueur + 1);
-        if (getch() == TOUCHE_ESPACE) // On entre 32 en constante, correspondant à ESPACE
-        {
-            joueur_i[id_joueur].position += lancer_de(&de1, &de2); // on lance les dés
-            nb_lancer++; // on augmente le nombre de 1 car les dés ont été lancé
-            if (nb_lancer == 3)
+        case TOUCHE_ESPACE:
+            while ((nb_lancer == 0) || ((de1 == de2)))
             {
-            // fonction prison, on interrompt le tour si c'est le troisième double, direct en prison
-            }
-            if (joueur_i[id_joueur].position >= 28) // on teste si le joueur arrive à la case départ ou non
-            {
-                printf("Vous etes passe par la case depart ! Vous recevez 200");
-                joueur_i[id_joueur].argent += 200;
-            }
-            joueur_i[id_joueur].position = joueur_i[id_joueur].position % 28; // modulo 28, pour faire un tour du plateau
+                //printf("C'est au joueur %d de jouer ! Appuyez sur ESPACE pour lancer les des\n", id_joueur + 1);
+                //if (getch() == TOUCHE_ESPACE) // On entre 32 en constante, correspondant à ESPACE
+                //{
+                    joueur_i[id_joueur].position += lancer_de(&de1, &de2); // on lance les dés
+                    nb_lancer++; // on augmente le nombre de 1 car les dés ont été lancé
+                    if (nb_lancer == 3)
+                    {
+                    // fonction prison, on interrompt le tour si c'est le troisième double, direct en prison
+                    }
+                    if (joueur_i[id_joueur].position >= 28) // on teste si le joueur arrive à la case départ ou non
+                    {
+                        printf("Vous etes passe par la case depart ! Vous recevez 200");
+                        joueur_i[id_joueur].argent += 200;
+                    }
+                    joueur_i[id_joueur].position = joueur_i[id_joueur].position % 28; // modulo 28, pour faire un tour du plateau
 
-            switch(joueur_i[id_joueur].position)
-            {
-                case 2:
-                    printf("Joueur %d, vous devez payez l'impot sur le revenu s'elevant a 200", id_joueur + 1);
-                    joueur_i[id_joueur].argent -= 200;
-                    break;
+                    switch(joueur_i[id_joueur].position)
+                    {
+                        case 2:
+                            printf("Joueur %d, vous devez payez l'impot sur le revenu s'elevant a 200", id_joueur + 1);
+                            joueur_i[id_joueur].argent -= 200;
+                            break;
 
-                case 5:
-                case 19:
-                    printf("chance !\n");
-                    break;
+                        case 5:
+                        case 19:
+                            printf("chance !\n");
+                            break;
 
-                case 12:
-                case 26:
-                    printf("communaute !\n");
-                    break;
+                        case 12:
+                        case 26:
+                            printf("communaute !\n");
+                            break;
 
-                case 21:
-                    printf("Allez en prison !\n");
-                    break;
+                        case 21:
+                            printf("Allez en prison !\n");
+                            break;
 
-                case 14:
-                    printf("Vous etes en stationnement gratuit ! Vous pouves soufflez le temps d'un tour !\n");
-                    break;
+                        case 14:
+                            printf("Vous etes en stationnement gratuit ! Vous pouves soufflez le temps d'un tour !\n");
+                            break;
 
-                default:
-                    tomber_sur_terrain(joueur_i, nombre_joueurs, id_joueur, cartes_terrain);
-                    break;
+                        default:
+                            tomber_sur_terrain(joueur_i, nombre_joueurs, id_joueur, cartes_terrain);
+                            break;
 
+                    if (de1 == de2)
+                    {
+                        printf("Vous avez fait double !\n");
+                    }
+                }
+            }
+            return joueur_i[id_joueur].argent;
 
-            }
-            /*if (joueur_i[id_joueur].position == 2)
-            {
-                printf("Joueur %d, vous devez payez l'impot sur le revenu s'elevant a 200", id_joueur + 1);
-                joueur_i[id_joueur].argent -= 200;
-                break;
-            }
-            else if (joueur_i[id_joueur].position == 5 || joueur_i[id_joueur].position == 19) /// carte chance
-            {
-                break;
-            }
-            else if (joueur_i[id_joueur].position == 12 || joueur_i[id_joueur].position == 26) /// carte communauté
-            {
-                break;
-            }
-            else if (joueur_i[id_joueur].position == 14) /// stationnement ou case départ*
-            {
-                printf("Vous etes en stationnement gratuit ! Vous pouves soufflez le temps d'un tour !\n");
-                break;
-            }
-            else
-            {
-                tomber_sur_terrain(joueur_i, nombre_joueurs, id_joueur, cartes_terrain);
-                break;
-            }
-            //deplacement_joueur(joueur_i[id_joueur], id_joueur);
-            // déplacement du joueur
-            /*if (tomber_sur_terrain)
-            {
-                acheter ou rien;
-            }
-            else if (tomber_sur_impot)
-            {
-                payer import
-            }
-            else if (tomber sur case départ ou stationnement gratuit)
-            {
-                rien faire ou gagner argent
-            }
+        case 'h':
+        case 'H':
+            break;
 
-            */
-            if (de1 == de2)
-            {
-                printf("Vous avez fait double !\n");
-            }
-        }
+        case 'v':
+        case 'V':
+            break;
+
     }
-    return joueur_i[id_joueur].argent;
 }
+
+
+    /// tour d'un joueur, ou autre lancer car dés doubles
